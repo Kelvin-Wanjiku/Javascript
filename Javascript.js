e48f76f84c490d56c5b325628d86162ab0c583af
kelvin-wanjiku <kelvinwanjiku249@gmail.com>
2022-05-02 10:31:23 -0700

    add code

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,20 @@
-var a;
-a=5;
-document.write(a)
 
-var demo;
-demo=kayvoh;
-document.write(demo)
+/* operators*/
+document.write("operators in javascript")
+document.write("<br>")
+x=10;
+y=5;
+z=x+y;
+document.write(z)
+document.write("<br>")
+
+a=x%y
+document.write(a)
+document.write("<br>")
+
+b=x > y
+document.write(b)
+document.write("<br>")
+
+
+
