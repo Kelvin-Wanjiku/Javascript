7da06842a36d05216ce590e66a3e12bfc4a62464
kelvin-wanjiku <kelvinwanjiku249@gmail.com>
2022-06-06 23:28:28 -0700

    adding code

--- functions/arrow_function.js
+++ functions/arrow_function.js
@@ -1,3 +1,3 @@
 const MyFunction = (a,b,c) =>{return a*b*c}
-let product = MyFunction(2,3,4)
-document.getElementById("function").innerHTML = product
+let product = MyFunction(2,3,4);
+document.getElementById("function").innerHTML = product;
